98a41443710677a1c5336d64d77347f5aa0d3e45
Thomas Riffard <ferdodo@hotmail.fr>
2020-03-24 11:15:24 +0100

    Add logging.

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var express = require("express");
 var Game = require('./lib/Game.js');
 var Player = require('./lib/Player.js');
 var WsServer = require('./lib/WsServer.js');
+var morgan = require('morgan');
 
 (async function main(){
 	try{
 		await new Promise(function startServer(resolve, reject){
 			var app = express();
+			app.use(morgan("tiny"));
 			app.use(express.static('dist'));
 			var wsServer = new WsServer(app);
 			var player = new Player(app);
